379e8bf385ed0f85892f8ca15dc84f402f8baa4d
waxie <97557771+waxiechc@users.noreply.github.com>
2024-09-11 22:07:16 +0300

    Add files via upload

---
+++ 1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+
+class ProgrammingLanguage
+{
+    public string Name { get; private set; }
+    public int Appeared { get; private set; }
+    public string Creator { get; private set; }
+
+    public ProgrammingLanguage(string name, int appeared, string creator)
+    {
+        Name = name;
+        Appeared = appeared;
+        Creator = creator;
+    }
+
+    public void PrintInfo()
+    {
+        Console.WriteLine($"Мова: {Name}, Рiк появи: {Appeared}, Автор: {Creator}");
+    }
+}
+
+class Languages
+{
+    public List<ProgrammingLanguage> LanguageList { get; private set; }
+
+    public Languages()
+    {
+        LanguageList = new List<ProgrammingLanguage>();
+    }
+
+    public void AddLanguage(ProgrammingLanguage lang)
+    {
+        LanguageList.Add(lang);
+    }
+
+    public void ShowAllLanguages()
+    {
+        foreach (var lang in LanguageList)
+        {
+            lang.PrintInfo();
+        }
+    }
+}
+
+static class XmlToLanguagesConverter
+{
+    public static Languages Convert(string xmlFilePath)
+    {
+        Languages languages = new Languages();
+
+        XmlDocument xmlDoc = new XmlDocument();
+        xmlDoc.Load(xmlFilePath);
+
+        // Проходимо по кожному елементу "lang"
+        foreach (XmlNode node in xmlDoc.DocumentElement.SelectNodes("lang"))
+        {
+            string name = node.Attributes["name"].Value;
+            int appeared = int.Parse(node["appeared"].InnerText);
+            string creator = node["creator"].InnerText;
+
+            // Додаємо мову в список
+            languages.AddLanguage(new ProgrammingLanguage(name, appeared, creator));
+        }
+
+        return languages;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Шлях до XML-файлу
+        string xmlFilePath = @"C:\languages.xml";
+
+        // Перетворюємо XML на об'єкт класу Languages
+        Languages languages = XmlToLanguagesConverter.Convert(xmlFilePath);
+
+        // Виводимо інформацію про всі мови
+        languages.ShowAllLanguages();
+    }
+}
---
+++ 2.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+
+class Personal
+{
+    public string Id { get; private set; }
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public string Nickname { get; private set; }
+    public decimal Salary { get; private set; }
+
+    public Personal(string id, string firstName, string lastName, string nickname, decimal salary)
+    {
+        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
+        Nickname = nickname;
+        Salary = salary;
+    }
+
+    public void PrintInfo()
+    {
+        Console.WriteLine($"ID: {Id}, Прiзвище: {LastName}, Iм'я: {FirstName}, Нiк: {Nickname}, Зарплата: {Salary}");
+    }
+}
+
+class Firma
+{
+    public List<Personal> PersonalList { get; private set; }
+
+    public Firma()
+    {
+        PersonalList = new List<Personal>();
+    }
+
+    public void AddPersonal(Personal person)
+    {
+        PersonalList.Add(person);
+    }
+
+    public void ShowAllPersonal()
+    {
+        foreach (var person in PersonalList)
+        {
+            person.PrintInfo();
+        }
+    }
+}
+
+static class XmlToFirmaConverter
+{
+    public static Firma Convert(string xmlFilePath)
+    {
+        Firma firma = new Firma();
+
+        XmlDocument xmlDoc = new XmlDocument();
+        xmlDoc.Load(xmlFilePath);
+
+        // Проходимо по кожному елементу "personal"
+        foreach (XmlNode node in xmlDoc.DocumentElement.SelectNodes("personal"))
+        {
+            string id = node.Attributes["id"].Value;
+            string firstName = node["firstname"].InnerText;
+            string lastName = node["lastname"].InnerText;
+            string nickname = node["nickname"].InnerText;
+            decimal salary = decimal.Parse(node["salary"].InnerText);
+
+            // Додаємо особу в список
+            firma.AddPersonal(new Personal(id, firstName, lastName, nickname, salary));
+        }
+
+        return firma;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Шлях до XML-файлу
+        string xmlFilePath = @"C:\firma.xml";
+
+        // Перетворюємо XML на об'єкт класу Firma
+        Firma firma = XmlToFirmaConverter.Convert(xmlFilePath);
+
+        // Виводимо інформацію про всіх працівників
+        firma.ShowAllPersonal();
+    }
+}
